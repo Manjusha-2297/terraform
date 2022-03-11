5a73ac8c0b24ad9400e7743e14d15b1d9cb9858d
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-11 14:53:30 +0530

    auto pushing chainges repo

--- 02-variables/variables.tf
+++ 02-variables/variables.tf
@@ -1,5 +1,16 @@
+//string data type
 variable "sample" {
  default = "hello"
+}
+
+//number data type
+variable "sample1" {
+ default = 100
+}
+
+//boolean
+variable "sample2" {
+ default = true
 }
 
 //for accessing the variable
@@ -10,4 +21,23 @@ output "sample" {
 // combine variable with other strings
 output "sample1" {
  value = "${var.sample}, welcome"
+}
+
+variable "new-list" {
+ default = ["Hello",100]
+}
+
+variable "training" {
+ default = {
+  course = "DevOps"
+  time = "6am"
+ }
+}
+
+output "list1" {
+ value = var.new-list[1]
+}
+
+output "map1" {
+ value = var.training["course"]
 }
