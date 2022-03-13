82daff5c0a4ea9f92e189297a62aac7f15345562
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-13 17:32:30 +0530

    auto pushing chainges repo

--- 08-provisioners/ec2.tf
+++ 08-provisioners/ec2.tf
@@ -3,23 +3,38 @@ provider "aws" {
 }
 
 resource "aws_instance" "sample" {
+  count         = 1
   ami           = "ami-0b90346fbb8e13c09"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
 
+#  provisioner "remote-exec" {
+#    connection {
+#      user = "centos"
+#      password = "DevOps321"
+#      host = self.public_ip
+#    }
+#
+#    inline = [
+#      "uptime"
+#    ]
+#  }
+}
+
+resource "null_resource" "remote" {
+  count = 1
   provisioner "remote-exec" {
+
     connection {
       user = "centos"
       password = "DevOps321"
-      host = self.public_ip
-    }
+      host = element(aws_instance.sample.*.public_ip, count.index)
 
-    inline = [
-      "uptime"
-    ]
+    }
   }
 }
+
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
