e59f213797933bc653bda981dde186f5707eb890
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-13 11:38:52 +0530

    auto pushing chainges repo

--- 03-ec2-with-sg/ec2.tf
+++ 03-ec2-with-sg/ec2.tf
@@ -5,4 +5,30 @@ provider "aws" {
 resource "aws_instance" "sample" {
   ami           = "ami-0b90346fbb8e13c09"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
