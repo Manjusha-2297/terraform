ec944852f759b29726ed9940777bfd7f862e49f7
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-13 12:57:12 +0530

    auto pushing chainges repo

--- 04-remote-state-s3/ec2.tf
+++ 04-remote-state-s3/ec2.tf
@@ -15,6 +15,6 @@ resource "aws_instance" "sample" {
   ami           = "ami-0b90346fbb8e13c09"
   instance_type = "t3.micro"
   tags          = {
-    Name = "sample"
+    Name        = "sample"
   }
 }
